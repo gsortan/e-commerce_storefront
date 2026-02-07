460e9a191e25574997796d5cd97307075312b588
Gordon Tan <gordonsortan@gmail.com>
2026-02-07 01:11:17 -0500

    Adding autocomplete for search bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,8 +4,13 @@ import Image from "next/image";
 import shopBanner from "../../public/ShopLocal.png";
 import SearchBar from "./SearchBar";
 import ProfileDropdown from "./ProfileDropdown";
+import {Autocomplete, AutocompleteItem} from "@heroui/react";
+import { getAllProducts } from "@/services/product/productServices";
 
 export default async function NavBar({ user }) {
+
+  const products = getAllProducts();
+
   return (
     <>
       <div className="p-4 grid grid-cols-[auto_1fr_auto]  items-center bg-[#232f3e] shadow-md  sticky top-0 z-30">
@@ -25,7 +30,7 @@ export default async function NavBar({ user }) {
           
         "
         >
-          <SearchBar />
+           <SearchBar/>
         </div>
 
         <div className="flex items-center gap-4 col-start-3 row-start-1">
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,33 +1,17 @@
+
 import { redirect } from "next/navigation";
-import { MdSearch } from "react-icons/md";
+import SearchBarClient from "./SearchBarClient";
+import { getAllProducts } from "@/services/product/productServices";
 
+export default async function SearchBar({ searchParams }) {
+  const products = await getAllProducts();
 
-export default function SearchBar() {
   async function searchAction(formData) {
     "use server";
     const q = (formData.get("q") || "").toString().trim();
     redirect(q ? `/shop?q=${encodeURIComponent(q)}` : "/shop");
   }
 
-  return (
-   
-    <form
-      action={searchAction}
-      className="flex w-full focus-within:ring-2 focus-within:ring-blue-500 focus-within:border-blue-500"
-    >
-      <input
-        name="q"
-        placeholder="Search items"
-        className="bg-white h-10 w-full px-4 outline-none"
-      />
-
-      <button
-        type="submit"
-        className="h-10 px-4 w-12 max-w-12 bg-gray-200 hover:bg-gray-300"
-      >
-        <MdSearch/>
-      </button>
-    </form>
-   
+  return (<SearchBarClient action={searchAction} items={products} />
   );
 }
